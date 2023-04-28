0fdd0e7492ca3c2ebf1604ad35cbb0fe3de97eca
Jorge <jjlosantos7@gmail.com>
2023-04-28 16:23:20 +0200

    Tarea 3

--- src/main/java/ed/jorge/Instituto.java
+++ src/main/java/ed/jorge/Instituto.java
@@ -2,14 +2,19 @@ package ed.jorge;
 
 public class Instituto {
 
+	private static final int jANYO = 2023;
+	private static final String jCICLO_FORMATIVO = "1º DAW";
+	private static final String jAPELLIDOS_ALUMNO = "Gregori";
+	private static final String jNOMBRE_ALUMNO = "Jorge";
+
 	public static void main(String[] args) {
 		
 		Alumno p = new Alumno();
 		
-		p.nombre = "Jorge";
-		p.apellidos = "Gregori";
-		p.cicloFormativo = "1º DAW";
-		p.anyo = 2023;
+		p.nombre = jNOMBRE_ALUMNO;
+		p.apellidos = jAPELLIDOS_ALUMNO;
+		p.cicloFormativo = jCICLO_FORMATIVO;
+		p.anyo = jANYO;
 		
 		System.out.println(p.toString());
 
